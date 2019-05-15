8016cee0fef89c1920ff42302ff98c2798a07120
Steve Farrugia <steve.farrugia@stratumn.com>
2019-05-15 11:46:18 +0200

    Init

---
+++ notes-on-trust/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Faster and worse | Notes on trust</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#EDEDEF">
+        <meta name="author" content="Stephen Farrugia">
+        <meta name="Title" content="Notes on trust">
+        <meta name="Description" content="Ideas and thoughts on the subject of trust">
+        <meta name="Keywords" content="Steve Farrugia, Stephen Farrugia, design, responsive, semantics">
+        <meta property="og:type" content="article">
+        <meta property="og:url" content="https://fasterandworse.com/notes-on-trust/">
+        <meta property="og:title" content="Notes on trust">
+        <meta property="og:description" content="Ideas and thoughts on the subject of trust">
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:domain" content="fasterandworse.com">
+        <meta name="twitter:title" content="Notes on trust">
+        <meta name="twitter:description" content="Ideas and thoughts on the subject of trust">
+        <meta name="twitter:url" content="https://fasterandworse.com/notes-on-trust/">
+        <link href="/main.css" rel="stylesheet" type="text/css" media="all" />
+    </head>
+    <body>
+        <article itemscope itemtype="http://schema.org/BlogPosting">
+            <header>
+                <h1 itemprop="title">Notes on trust</h1>
+                <time itemprop="dateCreated pubdate datePublished" datetime="2019-05-06" hidden>May 6, 2019</time>
+            </header>
+            <main>
+                <p>Asking a person to attest to their activity every time they do something is always going to make a process slower and more complicated.</p>
+                <p>The fix is to design the software so the attestations are automated and invisible.</p>
+                <p>Now the person is unaware of the weight of their actions and the subject of a surveillance system.</p>
+                <hr>
+                <p>Immutability is not a human goal, it is a goal of an enforcement mechanism.</p>
+                <hr>
+                <p>Trust is only a two-way desire when both parties are of equal power.</p>
+                <p>Everyone desires everyone else to be honest.</p>
+                <p>Desire for trust is inversely proportional to the balance of power.</p>
+                <p itemprop="author">Stephen Farrugia</p>
+            </main> 
+        </article>
+    </body>
+</html>
---
+++ the-how-is-personal/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Faster and worse | The how is personal</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#EDEDEF">
+        <meta name="author" content="Stephen Farrugia">
+        <meta name="Title" content="The how is personal">
+        <meta name="Description" content="Deadlines are important but trust makes the difference.">
+        <meta name="Keywords" content="Steve Farrugia, Stephen Farrugia, design, responsive, semantics">
+        <meta property="og:type" content="article">
+        <meta property="og:url" content="https://fasterandworse.com/the-how-is-personal/">
+        <meta property="og:title" content="The how is personal">
+        <meta property="og:description" content="Deadlines are important but trust makes the difference.">
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:domain" content="fasterandworse.com">
+        <meta name="twitter:title" content="The how is personal">
+        <meta name="twitter:description" content="Deadlines are important but trust makes the difference.">
+        <meta name="twitter:url" content="https://fasterandworse.com/the-how-is-personal/">
+        <link href="/main.css" rel="stylesheet" type="text/css" media="all" />
+    </head>
+    <body>
+        <article itemscope itemtype="http://schema.org/BlogPosting">
+            <header>
+                <h1 itemprop="title">The how is personal</h1>
+                <time itemprop="dateCreated pubdate datePublished" datetime="2019-03-27" hidden>March 27, 2019</time>
+            </header>
+            <main>
+                <p>Deadlines are important but trust makes the difference.</p>
+                <p>When someone trusts you to deliver, your only responsibility is to deliver.</p>
+                <p>The weight of the trust lays in your claim. You have to be very specific about what you will deliver. Articulate the smallest outputs of the mechanisms you are creating.</p>
+                <p>This leaves you open to creativity while holding you to your claim.</p>           
+                <p itemprop="author">Stephen Farrugia</p>
+            </main> 
+        </article>
+    </body>
+</html>
