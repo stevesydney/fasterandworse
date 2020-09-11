06356db6fc670372788b9fc384961bdb69bd9652
Steve Farrugia <steve.farrugia@stratumn.com>
2020-09-11 15:58:07 +0200

    Added scss - needs much more optimisation and modularisation

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,14 @@
+const pluginSass = require("eleventy-plugin-sass");
+const sassPluginOptions = {
+  sourcemaps: true
+};
 module.exports = eleventyConfig => {
+  eleventyConfig.addPlugin(pluginSass, sassPluginOptions);
   eleventyConfig.setTemplateFormats([
     "md",
     "html"
   ]);
   eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("favicon-16x16.png");
