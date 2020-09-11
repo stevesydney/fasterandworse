c351c05166e647e3de2866c5e585a0b44768fb41
Steve Linux <sfarrugia@gmail.com>
2020-09-11 18:22:38 +0200

    Re-enabled js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-// (function () {
-//   const hue = Math.floor(Math.random() * 360);
-//   const root = document.documentElement;
-//   const dark = window.matchMedia('(prefers-color-scheme: dark)');
-//   function luminosity(darkScheme, lightScheme) {
-//     if (dark.matches) return darkScheme;
-//     return lightScheme;
-//   }
-//   root.style.setProperty('--colour-link', `hsl(${hue},68%,${luminosity('80%', '40%')})`);
-//   root.style.setProperty('--colour-link-hover', `hsl(${hue},68%,${luminosity('80%', '40%')})`);
-//   root.style.setProperty('--colour-link-focus', `hsl(${hue},68%,${luminosity('90%', '30%')})`);
-//   root.style.setProperty('--colour-link-visited', `hsl(${hue},68%,${luminosity('70%', '60%')})`);
-//   root.style.setProperty('--colour-link-active', `hsl(${hue},68%,${luminosity('95%', '20%')})`);
-// })();
+(function () {
+  const hue = Math.floor(Math.random() * 360);
+  const root = document.documentElement;
+  const dark = window.matchMedia('(prefers-color-scheme: dark)');
+  function luminosity(darkScheme, lightScheme) {
+    if (dark.matches) return darkScheme;
+    return lightScheme;
+  }
+  root.style.setProperty('--colour-link', `hsl(${hue},68%,${luminosity('80%', '40%')})`);
+  root.style.setProperty('--colour-link-hover', `hsl(${hue},68%,${luminosity('80%', '40%')})`);
+  root.style.setProperty('--colour-link-focus', `hsl(${hue},68%,${luminosity('90%', '30%')})`);
+  root.style.setProperty('--colour-link-visited', `hsl(${hue},68%,${luminosity('70%', '60%')})`);
+  root.style.setProperty('--colour-link-active', `hsl(${hue},68%,${luminosity('95%', '20%')})`);
+})();
